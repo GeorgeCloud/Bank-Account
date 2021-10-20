c2a96779ae3ce7876edf6e4a3bb3574424f52ab0
George Ceja <Net.george.sky@gmail.com>
2021-10-20 16:03:11 -0700

    add dunder str to BankAccount class

--- bank_account.py
+++ bank_account.py
@@ -8,6 +8,13 @@ class BankAccount:
         self.account_number = int(''.join(["{}".format(randint(0, 9)) for num in range(0, 8)])) # 8 digit number, unique per account.
         self.balance        = 0
 
-
+    def __str__(self):
+        return f'''
+        Account Full Name:{self.full_name}
+        Account No.: {''.join(["*", "*", "*", "*"] + list(str(self.account_number))[:4])}
+        Balance: {self.balance}
+        '''
 
 george_account = BankAccount('George Ceja')
+
+print(george_account)
